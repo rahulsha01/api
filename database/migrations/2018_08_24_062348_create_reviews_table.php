98c877272418111dfa59abc4515172128c729b0d
Rahul Sharma <make.rahulsharma@gmail.com>
2018-08-24 13:05:58 +0530

    Migration Products And review Created

--- database/migrations/2018_08_24_062348_create_reviews_table.php
+++ database/migrations/2018_08_24_062348_create_reviews_table.php
@@ -15,6 +15,7 @@ class CreateReviewsTable extends Migration
     {
         Schema::create('reviews', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('product_id');
             $table->timestamps();
         });
     }
